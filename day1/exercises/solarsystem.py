adc1063b141abc0f04c1f0dcc77be99cc61b6f97
dorislee0309 <dorislee@berkeley.edu>
2015-06-09 16:22:34 -0400

    saved up to q6

---
+++ day1/exercises/solarsystem.py
@@ -0,0 +1,39 @@
+class Star(object):
+    def __init__(self,name="Sun",mass=1.,radius=1.):
+    	self.name = name
+    	self.mass = mass
+    	self.radius = radius
+    def __str__(self):
+    	return "{}:{} M_sun, {} R_sun".format(self.name,self.mass,self.radius)
+class Planet(object):
+    def __init__(self,day,radius, name="Earth",mass=1.):
+    	self.day = day
+    	self.name = name
+    	self.mass = mass
+    	self.radius = radius
+    	self.density=mass*5.97219e27/(4./3.*3.14*(637810000*radius)**3) #5.97219e27 convert M_sun to g
+    def __str__(self):
+    	return "{}: {} days, {} R_earth, {} M_sun, density = {} g/cc".format(self.name, self.day, self.radius,self.mass,self.density)
+
+class System(object):
+    def __init__(self,star, planets):
+    	self.star=star #Name of the central star
+    	self.planets=planets
+    	self.n_planets=len(planets)
+    def add_planet(self,new_planet):
+    	self.planets.append(new_planet)
+    	self.n_planets+=1
+    # def __getitem__(self,idx):
+    # 	print self.planets
+    	# for i in self.planets:
+    	# 	if i.name ==idx:
+    	# 		print i
+    	# return ""
+
+
+    def __str__(self):
+    	print  "Sol: {} M_sun, {} R_sun".format(self.star.mass,self.star.radius)
+    	for i in self.planets: 
+    		print i
+    	return ""
+
