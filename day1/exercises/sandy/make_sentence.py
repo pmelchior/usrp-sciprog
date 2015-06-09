7f1b982d0da91382396b07f975438a5f9cdc36c2
nmcstaff <nmcstaff@micheaux.Princeton.EDU>
2015-06-09 14:25:34 -0400

    did make_sentence

---
+++ day1/exercises/sandy/make_sentence.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+output = ""
+user_input = ""
+while True:
+	user_input = raw_input("enter a word (. ! ? to end): ")
+	
+	if user_input == "." or user_input == "?" or user_input == "!":
+		break
+
+	output = output +  " " + user_input
+
+final = output + user_input
+final = final[1:]
+print final
