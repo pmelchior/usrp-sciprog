931b37817b6c35c87c45ecb997a13262a663e8a8
Sihan Yuan <sihanyuan@nat-oitwireless-inside-vapornet100-a-27219.Princeton.EDU>
2015-06-10 14:13:09 -0400

    did a thing

--- day1/exercises/sandy-ex1/solarsystem.py
+++ day1/exercises/sandy-ex1/solarsystem.py
@@ -1,16 +1,11 @@
 import numpy as np
 
 class Star:
-	def __init__(self):
-		self.m = 1
-		self.r = 1
-		self.n = 'sun'
-	def __init__(self, mass, radius, name):
-		self.m = mass
-		self.r = radius
-		self.n = name
-	def __str__(self):
-		return self.n + ": mass = " + str(self.m) + " M_sun, radius = " + str(self.r) + " R_sun."
 
-half_sun = Star()
-print(half_sun)
+	def __init__(self, **kwargs):
+		self.__dict__.update(kwargs)
+	def __str__(self):
+		return ""
+
+half_sun = Star(mass=0.5, radius=0.5, name='Demisol')
+print half_sun.name
