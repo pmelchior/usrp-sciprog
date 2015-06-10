ed5d8bb2d2f21a04d8f4fd1ba10588d4dd7470a9
Bingjie Wang <b.wang@pitt.edu>
2015-06-09 21:01:05 -0400

    added solution 1

---
+++ day1/exercises/bingjie/make_sentence.py
@@ -0,0 +1,11 @@
+
+# coding: utf-8
+
+from __future__ import division
+
+sentence = []
+while not any(word in sentence for word in ['.', '!', '?']):
+    word = raw_input('Enter a word (. ! or ? to end):')
+    sentence += word
+    sentence += (' ')
+print ''.join(sentence)
