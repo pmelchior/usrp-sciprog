559cdfeb0eb5e9dcb94d08372dca01e6e7936f54
nmcstaff <nmcstaff@lasseter.Princeton.EDU>
2015-06-11 15:51:37 -0400

    setattr question

--- day1/exercises/solarsystem.py
+++ day1/exercises/solarsystem.py
@@ -1,5 +1,3 @@
-from math import pi
-
 class Star(object):
     def __init__(self, mass=1.0, radius=1.0, name='Sol'):
         """
@@ -10,22 +8,37 @@ class Star(object):
         self.name = name
 
     def __str__(self):
-        return '{}: {:.2f} M_sun, {:2f} R_sun'.format(self.name, self.mass, self.radius)
+        return '{}: {:.2f} M_sun, {:.2f} R_sun'.format(self.name, self.mass, 
+            self.radius)
  
 class Planet(object):
-    def __init__(self,period,radius,name,mass=radius**2):
+    def __init__(self,period,radius,name,mass=None):
         """
         Takes inputs period, radius, name, mass
         """
         self.period = period
-        self.mass = mass
+        if mass is None:
+            self.mass = radius**2
+        else: self.mass = mass
         self.radius = radius
-        self.density = self.mass / (4/3 * pi * self.radius**3)
+        self.density = 5.51 * self.mass / self.radius**3
         self.name = name
     
     def __str__(self):
-        return '{:0f}: {:2f} days, {:2f} R_earth, {:2f} M_earth, + \
-        density = {:1f} g/cc'.format(self.name,self.period,self.radius,self.mass)
+        return '{}: {:.0f} days, {:.2f} R_earth, {:.2f} M_earth, \
+density = {:.1f} g/cc'.format(self.name,self.period,self.radius,
+            self.mass,self.density)
 
-sun = Star()
-print(sun)
+    def __setattr__(self,name,value):
+        self.__dict__[name] = value
+        if (self.__dict__[name] != None) and ((name == 'mass') or (name == 'radius')):
+            self.__dict__[name] = value
+            self.density = 5.51 * self.mass / self.radius**3
+
+earth = Planet(365,1.0,mass = 1.0, name = "Earth")
+print(earth)
+venus = Planet(226,0.92,name="Venus")
+print(venus)
+print venus.mass, venus.density
+venus.mass = 0.81
+print venus.mass, venus.density
