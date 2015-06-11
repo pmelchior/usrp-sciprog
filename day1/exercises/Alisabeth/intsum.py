ba3590445885f17985cedad8052b246ca8206f26
Alisabeth Marsteller <aam3@orcus.astro.princeton.edu>
2015-06-11 10:17:03 -0400

    testing code

--- day1/exercises/Alisabeth/intsum.py
+++ day1/exercises/Alisabeth/intsum.py
@@ -9,23 +9,24 @@ def int_sum(x):
     while z < len(x):
         number = x[z]
         sum1 += int(number)
-        z += 1
+        z += 1    
     return sum1
 
 import sys
 x = sys.argv[1]
 limit = int(x)**2
+print(len(sys.argv))
 
 if len(sys.argv) < 3:
     while x <= limit:
         sum2 = int_sum(x)
         print(sum2)
         x += x
-        break
+
 else:
     y = sys.argv[2]
     while x <= y:
         sum2 = int_sum(x)
         print(sum2)
         x += x
-        break
+   
