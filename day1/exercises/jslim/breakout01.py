9a9481d4b06177e6d705edfdbdb508a3e97fb14b
jinsoo601 <jslim@princeton.edu>
2015-06-11 11:13:11 -0400

    fixed breakout01.py

--- day1/exercises/jslim/breakout01.py
+++ day1/exercises/jslim/breakout01.py
@@ -2,14 +2,19 @@
 
 def askword():
 
-	sentence = ''
-	end = True
-	while end:
-		word = raw_input('Enter a Word (. ! or ? to end): ')
-		sentence += word + ' '
-		if word in ['.', '!', '?']: 
-			end = False
-	print sentence
+    sentence = []
+    end = True
+    while end:
+        word = raw_input('Enter a Word (. ! or ? to end): ')
+        sentence.append(word)
+        if word in ['.', '!', '?']: 
+            end = False
+    excl = sentence.pop()
+    last_word = sentence.pop()
+    sentence_string = ''
+    for x in sentence:
+        sentence_string += x + ' '
+    print (sentence_string + last_word + excl)      
 
 if __name__ == '__main__':
-	askword()
+    askword()
