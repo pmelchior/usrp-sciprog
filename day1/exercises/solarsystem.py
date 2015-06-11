62f594f22643f8d6f7cb0da8644cc93cc75063d0
nmcstaff <nmcstaff@lasseter.Princeton.EDU>
2015-06-11 16:21:39 -0400

    progress on ex6

--- day1/exercises/solarsystem.py
+++ day1/exercises/solarsystem.py
@@ -21,24 +21,38 @@ class Planet(object):
             self.mass = radius**2
         else: self.mass = mass
         self.radius = radius
-        self.density = 5.51 * self.mass / self.radius**3
         self.name = name
     
     def __str__(self):
         return '{}: {:.0f} days, {:.2f} R_earth, {:.2f} M_earth, \
 density = {:.1f} g/cc'.format(self.name,self.period,self.radius,
             self.mass,self.density)
+    
+    @property
+    def density(self):
+        return 5.51 * self.mass / self.radius**3
 
-    def __setattr__(self,name,value):
-        self.__dict__[name] = value
-        if (self.__dict__[name] != None) and ((name == 'mass') or (name == 'radius')):
-            self.__dict__[name] = value
-            self.density = 5.51 * self.mass / self.radius**3
+class System(object):
+    def __init__(self,star,planets):
+        """
+        Takes a Star object and a list of Planet objects and creates
+        a System object
+        """
+        self.star = star
+        self.planets = planets
 
-earth = Planet(365,1.0,mass = 1.0, name = "Earth")
-print(earth)
-venus = Planet(226,0.92,name="Venus")
-print(venus)
-print venus.mass, venus.density
-venus.mass = 0.81
-print venus.mass, venus.density
+    def __str__(self):
+        return '{}: {:.2f} M_sun, {:.2f} R_sun'.format(self.star.name, self.star.mass, 
+            self.star.radius)
+
+    def n_planets(self):
+        return len(self.planets)
+
+    def add_planet(self,planet):
+        self.planets = self.planets.append(planet)
+
+
+sun = Star()
+half_sun = Star(mass=0.5, radius=0.5, name='Demisol')
+earth = Planet(365, 1.0, mass=1.0, name='Earth')
+venus = Planet(226, 0.92, name='Venus')
