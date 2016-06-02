853e13f2327fa11c955fe8b39c898ff06dd2a2d9
Peter Melchior <peter.m.melchior@gmail.com>
2016-06-02 17:21:44 -0400

    added hubble project with table and coordinate conversion

---
+++ projects/hubble/coordinates.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def Dec2Deg(dec):
+    try:
+        assert not isinstance(dec, basestring)
+        coord_deg = np.empty(len(dec))
+        for i in xrange(len(dec)):
+            degs, mins, secs = dec[i].split(":")
+            coord_deg[i] = float(degs) + float(mins)/60 + float(secs)/3600
+        return coord_deg
+    except AssertionError:
+        return Dec2Deg([dec])[0]
+
+
+def Ra2Deg(ra):
+    return 15*Dec2Deg(ra)
