85c361641b640c0d84c12b78e736a8916c437c97
Timothy Morton <tim.morton@gmail.com>
2016-06-09 11:39:32 -0400

    added circle exercise

--- day3/exercises/tim/rotation.py
+++ day3/exercises/tim/rotation.py
@@ -5,5 +5,11 @@ def rotation(angle):
     a = np.deg2rad(angle)
     return np.array([[np.cos(a),np.sin(a)],[-np.sin(a), np.cos(a)]])
 
+def circle(npoints=18):
+    """Returns (x,y) coordinates for npoints around circle
+    """
+    angles = np.deg2rad(np.linspace(0,360,npoints,False))
+    return np.array([[np.cos(a), np.sin(a)] for a in angles])
+
 if __name__=='__main__':
     print(rotation(30))
