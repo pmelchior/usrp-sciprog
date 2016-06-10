0ccac2bce39e602cb88a418486f9db05e9b9d67e
Peter Melchior <peter.m.melchior@gmail.com>
2016-06-10 14:55:01 -0400

    made coordinates work in python3

--- projects/hubble/coordinates.py
+++ projects/hubble/coordinates.py
@@ -1,10 +1,13 @@
+import numpy as np
+
 def Dec2Deg(dec):
     import numpy as np
+    from six import string_types
     try:
-        assert not isinstance(dec, basestring)
+        assert not isinstance(dec, string_types)
         coord_deg = np.empty(len(dec))
-        for i in xrange(len(dec)):
-            degs, mins, secs = dec[i].split(":")
+        for i in range(len(dec)):
+            degs, mins, secs = dec[i].decode().split(":")
             coord_deg[i] = float(degs) + float(mins)/60 + float(secs)/3600
         return coord_deg
     except AssertionError:
