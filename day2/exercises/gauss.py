58369c4f92661a79095e12a17d872d37d609b6ad
Charles Stahl <cnstahl@princeton.edu>
2017-06-15 14:33:47 -0400

    finished gaussian

--- day2/exercises/gauss.py
+++ day2/exercises/gauss.py
@@ -1,6 +1,29 @@
-#! /Users/cnstahl/anaconda3/bin/python
+#!/usr/bin/env python
 
-def f(x, y, mean, cov):
-    f = np.zeros(len(x))
-    for i in range(len(x)):
-        
+import numpy as np
+
+def gaussian2D(x, y, mean, cov):
+    x_var = x - mean[0]
+    y_var = y - mean[1]
+
+    vals = np.vstack((x_var, y_var))
+    dot  = np.dot(np.linalg.inv(cov).T, vals)
+    prod = dot * vals
+
+    return np.sum(prod, axis = 0)
+    
+import matplotlib.pyplot as plt
+
+x = np.arange(0.,10.,0.1)
+y = np.arange(0.,10.,0.1)
+x,y = np.meshgrid(x,y)
+mean_xy = np.array([5.,4.])
+cov = np.array([[1.,1.],
+                [1.,2.]])**2.
+
+f = gaussian2D(x.ravel(), y.ravel(), mean_xy, cov)
+f.reshape(100,100)
+
+plt.imshow(f.reshape(100,100))
+plt.show()
+plt.savefig('pic.pdf')
