496b29f2e503d681e6797187645f2106313038a4
Teva Ilan <tevailan@nat-oitwireless-inside-vapornet100-c-20250.Princeton.EDU>
2017-06-16 13:37:02 -0400

    added plot function

--- projects/transit/group3/transit.py
+++ projects/transit/group3/transit.py
@@ -9,7 +9,12 @@ def read_data(ob_num):
     return(t,f)
     pass
 
-def plot_data():
+def plot_data(ob_num):
+    with open('../data/'+str(ob_num)+'.txt') as f: 
+        data= np.loadtxt(f, dtype=float)
+    t=data[:,0]
+    f=data[:,1]
+    plt.scatter(t,f)
     pass
 
 def trapezoid():
