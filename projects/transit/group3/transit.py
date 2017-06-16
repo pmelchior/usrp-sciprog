1e3cb9a5ef55cc0908396ab917b2a5973406ec7a
Teva Ilan <tevailan@nat-oitwireless-inside-vapornet100-c-20250.Princeton.EDU>
2017-06-16 13:29:27 -0400

    whatever

--- projects/transit/group3/transit.py
+++ projects/transit/group3/transit.py
@@ -1,30 +1,35 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def read_data:
+def read_data(ob_num):
+    with open('../data/'+str(ob_num)+'.txt') as f: 
+        data= np.loadtxt(f, dtype=float)
+    t=data[:,0]
+    f=data[:,1]
+    return(t,f)
     pass
 
-def plot_data:
+def plot_data():
     pass
 
-def trapezoid:
+def trapezoid():
     pass
 
-def vary_depth:
+def vary_depth():
     pass
 
-def vary_duration:
+def vary_duration():
     pass
 
-def vary_tau:
+def vary_tau():
     pass
 
-def vary_t0:
+def vary_t0():
     pass
 
-def plot_fit:
+def plot_fit():
     pass
 
-def fit_trapezoid:
+def fit_trapezoid():
     pass
 
