68e354ce4271449865d52e5f69fecff929c1bd71
Charles Stahl <cnstahl@princeton.edu>
2017-06-16 11:14:30 -0400

    refined covariance matrix

--- day2/exercises/gauss.py
+++ day2/exercises/gauss.py
@@ -18,8 +18,12 @@ x = np.arange(0.,10.,0.1)
 y = np.arange(0.,10.,0.1)
 x,y = np.meshgrid(x,y)
 mean_xy = np.array([5.,4.])
-cov = np.array([[1.,0.],
-                [0.,3.]])**2.
+evals = np.array([[3.,0.],
+                  [0.,1.]])**2.
+angle = 0 * np.pi
+evecs = np.array([[np.cos(angle), -np.sin(angle)],
+                  [np.sin(angle),  np.cos(angle)]])
+cov   = evecs.T @ evals @ evecs
 
 f = gaussian2D(x.ravel(), y.ravel(), mean_xy, cov)
 f.reshape(100,100)
