366a41482e848873ed80db70458143ccfba01869
Charles Stahl <cnstahl@princeton.edu>
2017-06-16 10:29:18 -0400

    progress on gauss

--- day2/exercises/gauss.py
+++ day2/exercises/gauss.py
@@ -7,7 +7,7 @@ def gaussian2D(x, y, mean, cov):
     y_var = y - mean[1]
 
     vals = np.vstack((x_var, y_var))
-    dot  = np.dot(np.linalg.inv(cov).T, vals)
+    dot  = np.dot(np.linalg.inv(cov), vals)
     prod = dot * vals
 
     return np.sum(prod, axis = 0)
@@ -18,12 +18,12 @@ x = np.arange(0.,10.,0.1)
 y = np.arange(0.,10.,0.1)
 x,y = np.meshgrid(x,y)
 mean_xy = np.array([5.,4.])
-cov = np.array([[1.,1.],
-                [1.,2.]])**2.
+cov = np.array([[1.,0.],
+                [0.,3.]])**2.
 
 f = gaussian2D(x.ravel(), y.ravel(), mean_xy, cov)
 f.reshape(100,100)
 
 plt.imshow(f.reshape(100,100))
-plt.show()
 plt.savefig('pic.pdf')
+plt.show()
