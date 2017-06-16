db02c1435e5a8e1f385727ab090a0162f640dd12
Charles Stahl <cnstahl@princeton.edu>
2017-06-16 12:04:07 -0400

    started transit

---
+++ projects/transit/group3/transit.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def read_data:
+    pass
+
+def plot_data:
+    pass
+
+def trapezoid:
+    pass
+
+def vary_depth:
+    pass
+
+def vary_duration:
+    pass
+
+def vary_tau:
+    pass
+
+def vary_t0:
+    pass
+
+def plot_fit:
+    pass
+
+def fit_trapezoid:
+    pass
+
