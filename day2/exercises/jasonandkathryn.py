ebecf2e70701a9b6ee9eb2764b34edc0ad51add1
Jason Kim <jjk7@princeton.edu>
2018-06-13 16:49:56 -0400

    Add files via upload

---
+++ day2/exercises/jasonandkathryn.py
@@ -0,0 +1,38 @@
+import numpy as np
+from math import sqrt
+
+def getPoints(numPoints):
+	randArr = np.random.uniform(-1, 1, (numPoints, 2))
+
+	mask=[]
+
+	for i in range(0, numPoints):
+		mask.append(distanceCheck(randArr[i][1],randArr[i][0]))
+
+	return mask
+
+
+def distanceCheck(x, y):
+	distance = sqrt(x**2 + y**2)
+	return distance < 1 # returns false if outside circle
+
+def truCounter(numPoints):
+	truArray = getPoints(numPoints)
+
+	numTrue = 0
+
+	for i in range(0, numPoints):
+		if truArray[i] == True:
+			numTrue = numTrue + 1
+	return numTrue
+
+def estimatePi(numPoints):
+	truCount = truCounter(numPoints)
+	return 4 * truCount / (numPoints)
+
+print(estimatePi(10000000))
+
+
+
+
+
