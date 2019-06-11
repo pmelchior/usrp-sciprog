8b12e244ac56a0f8ea98dde2d3b7b26e533ca982
herjy <remy.joseph11@gmail.com>
2019-06-11 00:42:08 -0400

    update on day2 readme

--- day2/arguments.py
+++ day2/arguments.py
@@ -7,7 +7,7 @@ if len(sys.argv) > 1:
 	for c, arg in enumerate(sys.argv[1:]):
 		print('Please give the description of input {0} with value {1}'.format(c, arg))
 		description = input()
-		print('input number {0}, with value {1} is {2}'.format(str(c), arg, description))
+		print('input number {0}, with value {1} is a {2}'.format(str(c), arg, description))
 else:
 	print('No argument was given to me, byebye now.')
 
