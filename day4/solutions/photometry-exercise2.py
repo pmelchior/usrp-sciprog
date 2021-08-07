17a556e8cd5162e1dee8330391f276aaa105ff1b
removedporn <86824510+removedporn@users.noreply.github.com>
2021-08-08 07:45:10 +0800

    Delete day4 directory

--- day4/solutions/photometry-exercise2.py
+++
@@ -1,5 +0,0 @@
-from astropy.modeling.functional_models import Gaussian2D
-X, Y = np.meshgrid(np.arange(33), np.arange(33))
-
-model = Gaussian2D.evaluate(X, Y, 1, 16, 16, 3, 3, 0)
-plt.imshow(model)
