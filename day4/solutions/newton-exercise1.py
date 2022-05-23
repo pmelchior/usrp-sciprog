596a7791fdd388c98f11504dc744ded3b1d2c96c
rteyssier <romain.teyssier@gmail.com>
2022-05-23 11:03:14 -0400

    program updated

--- day4/solutions/newton-exercise1.py
+++
@@ -1,9 +0,0 @@
-%matplotlib inline
-import numpy as np
-import scipy.optimize as so
-import matplotlib.pyplot as plt
-
-result = so.minimize_scalar(f, bracket=(-10,10))
-x = np.linspace(-10,10,100)
-plt.plot(x, f(x))
-plt.scatter([result.x], [result.fun], c='r',zorder=10)
--- day4/solutions/photometry-exercise4-2.py
+++
@@ -1,7 +0,0 @@
-f, s, xm, ym, w = result.x
-theta = 0
-model = Gaussian2D.evaluate(X, Y, f, xm, ym, w, w, theta) + s
-
-residual = img - model
-plt.imshow(residual)
-plt.colorbar()
